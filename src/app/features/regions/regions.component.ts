c673dfea1c735b9ae05123be8bc047f97984e8e3
eldaniii <dvilmar309@g.educaand.es>
2025-02-19 22:09:06 +0100

    Commit 2

--- src/app/features/regions/regions.component.ts
+++ src/app/features/regions/regions.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegionService } from '../../core/services/region.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-regions',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './regions.component.html',
   styleUrl: './regions.component.scss'
 })
-export class RegionsComponent {
+export class RegionsComponent implements OnInit {
+  regions: any[] = [];
+  error: string | null = null;
 
+  constructor(private regionService: RegionService, private router: Router) {}
+
+  ngOnInit() {
+    this.regionService.fetchRegions().subscribe({
+      next: (res: any) => (this.regions = res),
+
+      error: (err) => {
+        if (err.status === 403) {
+          this.router.navigate(['/forbidden']);
+          } else {
+            this.error = 'An error occurred';
+          }
+      },
+      });
+  }
 }
